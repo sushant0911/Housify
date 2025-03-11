a251e8e1a03185877f7b9f8a0a2831b74b287404
@sushant0911 <sushantgupta.brg2003@gmail.com>
2025-03-11 22:37:54 +0530

    Sidebar Update Color

--- client/src/components/ProfileMenu/ProfileMenu.jsx
+++ client/src/components/ProfileMenu/ProfileMenu.jsx
@@ -1,32 +1,42 @@
-import React from 'react'
-import {Avatar, Menu} from '@mantine/core'
-import { useNavigate } from 'react-router-dom'
-const ProfileMenu = ({user, logout}) => {
-    const navigate = useNavigate()
-  return (
-    <Menu>
-        <Menu.Target>
-            <Avatar src={user?.picture} alt='user image' radius={"xl"}/>
-        </Menu.Target>
-        <Menu.Dropdown>
-            <Menu.Item {{backgroundColor : "white"}} onClick={()=> navigate("./favourites", {replace: true})}>
-                Favourites
-            </Menu.Item>
+import React from 'react';
+import { Avatar, Menu } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
 
-            <Menu.Item style = {{backgroundColor : "white"}} onClick={()=> navigate("./bookings", {replace: true})}>
-                Bookings
-            </Menu.Item>
+const ProfileMenu = ({ user, logout }) => {
+    const navigate = useNavigate();
 
-            <Menu.Item style = {{backgroundColor : "white"}} onClick={()=>{
-                localStorage.clear();
-                logout()
-            }}>
-                Logout
-            </Menu.Item>
-        </Menu.Dropdown>
-    </Menu>
-  )
-}
-//
+    return (
+        <Menu>
+            <Menu.Target>
+                <Avatar src={user?.picture} alt="user image" radius="xl" />
+            </Menu.Target>
+            <Menu.Dropdown>
+                <Menu.Item 
+                    style={{ backgroundColor: "white" }} 
+                    onClick={() => navigate("./favourites", { replace: true })}
+                >
+                    Favourites
+                </Menu.Item>
 
-export default ProfileMenu
+                <Menu.Item 
+                    style={{ backgroundColor: "white" }} 
+                    onClick={() => navigate("./bookings", { replace: true })}
+                >
+                    Bookings
+                </Menu.Item>
+
+                <Menu.Item 
+                    style={{ backgroundColor: "white" }} 
+                    onClick={() => {
+                        localStorage.clear();
+                        logout();
+                    }}
+                >
+                    Logout
+                </Menu.Item>
+            </Menu.Dropdown>
+        </Menu>
+    );
+};
+
+export default ProfileMenu;
